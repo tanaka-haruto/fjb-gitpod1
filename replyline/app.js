458350317df2e8788eea1c89e06a71a7dbe641ef
tanaka-haruto <x20u029@chiba-fjb.ac.jp>
2022-07-06 07:31:33 +0000

    とりい万歳！

---
+++ replyline/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const express = require('express');
+const line = require('@line/bot-sdk');
+const PORT = process.env.PORT || 3000;
+
+const config = {
+    channelSecret: 'f1d0b19809fc0b9bf07685cafe566362',
+    channelAccessToken: 'H4DCXEpl5ee0BQOej/icday5yBRVcTY0pEU6N3Mq2VW5zzbgNcq0TRn+0sbTwZWrkvRw9GJpu3ZI2qq5gSnWMyldBuVZ8Na2D6VnKDtTvokYOgJTG2EecdvvbxOODYcfdCTyFaf7pNc+objR6iVBRQdB04t89/1O/w1cDnyilFU='
+};
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); //ブラウザ確認用(無くても問題ない)
+app.post('/webhook', line.middleware(config), (req, res) => {
+    console.log(req.body.events);
+
+    //ここのif分はdeveloper consoleの"接続確認"用なので削除して問題ないです。
+    if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
+        res.send('Hello LINE BOT!(POST)');
+        console.log('疎通確認用');
+        return; 
+    }
+
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+
+async function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text //実際に返信の言葉を入れる箇所
+  });
+}
+
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
